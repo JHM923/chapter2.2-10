fb0544580c773069b351e144fd79d27cc1971158
username <1151555861@qq.com>
2021-06-28 17:51:08 +0800

    first commit

---
+++ chapter2.2-10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace chapter2._2_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> noValue = new Nullable<int>();
+            //Console.WriteLine(noValue.GetType());
+
+            Nullable<int> someValue = new Nullable<int>(5);
+            Console.WriteLine(someValue.GetType());
+            Console.ReadKey();
+        }
+    }
+}
